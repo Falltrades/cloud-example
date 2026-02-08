2f7d27f8d9f455762a763a75d2d49fef6e3363e7
falltrades <thaikhuevincent.nguyen@gmail.com>
2026-02-08 19:02:47 +0100

    feat: :sparkles: aws-ec2 (two-instances) html-db-website

---
+++ html-db-website/aws-ec2/two-instances/terraform/main.tf
@@ -0,0 +1,42 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_instance" {
+  ami                                  = data.aws_ami.ubuntu.id
+  instance_type                        = "t3.micro"
+  availability_zone                    = "us-east-1a"
+  subnet_id                            = module.vpc.private_subnet_objects[0].id
+  vpc_security_group_ids               = [aws_security_group.app_sg.id]
+  associate_public_ip_address          = false
+  iam_instance_profile                 = aws_iam_instance_profile.ssm.name
+  instance_initiated_shutdown_behavior = "terminate"
+
+  tags = {
+    Name    = "app_instance"
+    Project = "html-db"
+  }
+}
+
+resource "aws_instance" "db_instance" {
+  ami                                  = data.aws_ami.ubuntu.id
+  instance_type                        = "t3.micro"
+  availability_zone                    = "us-east-1a"
+  subnet_id                            = module.vpc.private_subnet_objects[0].id
+  vpc_security_group_ids               = [aws_security_group.db_sg.id]
+  associate_public_ip_address          = false
+  iam_instance_profile                 = aws_iam_instance_profile.ssm.name
+  instance_initiated_shutdown_behavior = "terminate"
+
+  tags = {
+    Name    = "db_instance"
+    Project = "html-db"
+  }
+}
