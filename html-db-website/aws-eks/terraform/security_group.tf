b8aa1c00f3d9e67615ec8a172aa8a9630125ae6a
falltrades <thaikhuevincent.nguyen@gmail.com>
2026-02-14 13:10:25 +0100

    feat: :sparkles: aws-eks html-db-website

---
+++ html-db-website/aws-eks/terraform/security_group.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "alb" {
+  name        = "nginx-alb-sg"
+  vpc_id      = aws_vpc.eks.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "alb_to_nodes" {
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = aws_eks_cluster.nginx.vpc_config[0].cluster_security_group_id
+  source_security_group_id = aws_security_group.alb.id # From the previous step
+}
